f1273f327bb0cb0aaf59edb0543ff3af376df5ca
ALLSONNIE <allibesa28@gmail.com>
2023-05-25 15:40:59 -0600

    Creación de los componentes para los cards (mejores recetas y todas las recetas)

---
+++ dist/js/components/AllRecipes.js
@@ -0,0 +1,51 @@
+app.component('all-recipes', {
+    props: {
+        image: {
+            type: String
+        },
+        name: {
+            type: String
+        },
+        category: {
+            type: String
+        },
+        totaltime: {
+            type: String
+        },
+        level: {
+            type: String
+        },
+        index: {
+            type: Number
+        },
+        
+    },
+    data() {
+        return {
+            counter: 0
+        }
+    },
+    mounted() {
+
+    },
+    methods: {
+        onClickViewRecipe(){
+            console.log("VIEW");
+            this.$emit('recipedetails', this.index);
+            //this.$test.emit('foo', this);
+        }
+    },
+    template:
+        /*html*/
+        `<div class="d-flex card card-style mb-5">
+        <img v-bind:src="image" class="img-cards" alt="featured recipe">
+        <div class="card-body m-2 padding-card">
+            <h2 class="cards-title mb-1">{{ name }}</h2>
+            <p class="cards-text mb-1">{{ category }}</p>
+            <p class="cards-text mb-1">{{ totaltime }}</p>
+            <p class="cards-text mb-1">{{ level }}</p>
+            <button class="btn btn-view text-center mt-3" v-on:click="onClickViewRecipe()"
+                data-bs-toggle="modal" data-bs-target="#staticBackdrop">Ver receta</button>
+        </div>
+    </div>`
+})
--- dist/js/main.js
+++ dist/js/main.js
@@ -3,7 +3,30 @@ const app = Vue.createApp({
     data() {
         return {
             selectedIndex: 0,
-            recipes: [ //array
+            bestRecipes: [ //array
+            //objetos
+                {id:102, image:"./images/pic2.jpg", name:"Hamburguesas", preptime:"10 minutos", cooktime:"10 minutos", totaltime:"20 minutos", portions:"8 porciones", category:"Almuerzo", occasion:"Todas", level:"Fácil", likes: 4, description:"Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.", ingredients:"Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat.", steps:"Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula vulputate sem tristique cursus."},
+                
+                {id:103, image:"./images/pic3.jpg", name:"Champiñones", preptime:"10 minutos", cooktime:"10 minutos", totaltime:"20 minutos", portions:"8 porciones", category:"Almuerzo", occasion:"Todas", level:"Intermedio", likes: 2, description:"Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.", ingredients:"Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat.", steps:"Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula vulputate sem tristique cursus."},
+                
+                {id:104, image:"./images/pic4.jpeg", name:"Tiramisú", preptime:"10 minutos", cooktime:"10 minutos", totaltime:"20 minutos", portions:"8 porciones", category:"Postre", occasion:"Todas", level:"Avanzado", likes: 3, description:"Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.", ingredients:"Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat.", steps:"Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula vulputate sem tristique cursus."},
+
+                {id:105, image:"./images/pic5.jpg", name:"Soufflé de chocolate", preptime:"10 minutos", cooktime:"10 minutos", totaltime:"20 minutos", portions:"8 porciones", category:"Postre", occasion:"Todas", level:"Fácil", likes: 3, description:"Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.", ingredients:"Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat.", steps:"Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula vulputate sem tristique cursus."},
+
+                {id:106, image:"./images/pic6.jpg", name:"Spaghetti", preptime:"10 minutos", cooktime:"10 minutos", totaltime:"20 minutos", portions:"8 porciones", category:"Almuerzo", occasion:"Todas", level:"Fácil", likes: 3, description:"Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.", ingredients:"Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat.", steps:"Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula vulputate sem tristique cursus."},
+
+                {id:107, image:"./images/pic7.jpg", name:"Ensalada de huevo", preptime:"10 minutos", cooktime:"10 minutos", totaltime:"20 minutos", portions:"8 porciones", category:"Almuerzo", occasion:"Todas", level:"Fácil", likes: 3, description:"Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.", ingredients:"Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat.", steps:"Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula vulputate sem tristique cursus."},
+
+                {id:108, image:"./images/pic8.jpg", name:"Rompope", preptime:"10 minutos", cooktime:"10 minutos", totaltime:"20 minutos", portions:"8 porciones", category:"Bebida", occasion:"Todas", level:"Fácil", likes: 3, description:"Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.", ingredients:"Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat.", steps:"Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula vulputate sem tristique cursus."},
+                
+                {id:109, image:"./images/pic9.jpg", name:"Sopa de espinacas", preptime:"10 minutos", cooktime:"10 minutos", totaltime:"20 minutos", portions:"8 porciones", category:"Sopas", occasion:"Todas", level:"Fácil", likes: 3, description:"Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.", ingredients:"Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat.", steps:"Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula vulputate sem tristique cursus."},
+                
+                {id:108, image:"./images/pic8.jpg", name:"Rompope", preptime:"10 minutos", cooktime:"10 minutos", totaltime:"20 minutos", portions:"8 porciones", category:"Bebida", occasion:"Todas", level:"Fácil", likes: 3, description:"Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.", ingredients:"Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat.", steps:"Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula vulputate sem tristique cursus."},
+                
+                {id:109, image:"./images/pic9.jpg", name:"Sopa de espinacas", preptime:"10 minutos", cooktime:"10 minutos", totaltime:"20 minutos", portions:"8 porciones", category:"Sopas", occasion:"Todas", level:"Fácil", likes: 3, description:"Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.", ingredients:"Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat.", steps:"Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula vulputate sem tristique cursus."}
+            
+            ],
+            allRecipes: [ //array
             //objetos
                 {id:102, image:"./images/pic2.jpg", name:"Hamburguesas", preptime:"10 minutos", cooktime:"10 minutos", totaltime:"20 minutos", portions:"8 porciones", category:"Almuerzo", occasion:"Todas", level:"Fácil", likes: 4, description:"Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.", ingredients:"Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat.", steps:"Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula vulputate sem tristique cursus."},
                 
@@ -56,3 +79,8 @@ const app = Vue.createApp({
     }
     
 });
+
+//init custom events for components
+const emitter = mitt();
+//global property for custom events
+app.config.globalProperties.$test = emitter; 
